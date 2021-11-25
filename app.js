2700123c8facd5dd25434c95450f835644171bd6
benyamin-rafaeli <benyamin.rafaeli@gmail.com>
2021-11-25 06:44:21 +0200

    test: mongo

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-// test
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+const app = express()
+const PORT = config.get('port') || 5000
 
-console.log('adfsadfasdfsfsf')
+async function start(){
+  try {
+    await mongoose.connect(config.get('mongoURI'))
+  } catch (e) {
+    console.log('Server Error', e.message);
+    process.exit(1);
+  }
+}
+start()
+app.listen(PORT, ()=> console.log(`app started on port ${PORT}`));
