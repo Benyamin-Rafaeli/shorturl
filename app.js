87d92b7aa6a4190266bafb69bb2ae69f3e89b947
benyamin-rafaeli <benyamin.rafaeli@gmail.com>
2021-11-25 07:07:14 +0200

    test: .prettierrc.json

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-const app = express()
-const PORT = config.get('port') || 5000
+const app = express();
+const PORT = config.get('port') || 5000;
 
-async function start(){
+app.use('/api/auth', require('./routes/auth.routes'));
+
+async function start() {
   try {
-    await mongoose.connect(config.get('mongoURI'))
+    await mongoose.connect(config.get('mongoURI'));
   } catch (e) {
     console.log('Server Error', e.message);
     process.exit(1);
   }
 }
-start()
-app.listen(PORT, ()=> console.log(`app started on port ${PORT}`));
+start();
+app.listen(PORT, () => console.log(`app started on port ${PORT}`));
