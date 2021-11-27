6c81ec23ee9592004f6410b56ae86a0fea897af4
benyamin-rafaeli <benyamin.rafaeli@gmail.com>
2021-11-27 07:00:44 +0200

    test: finised autorisation routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,79 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { checks, validationResult, check } = require('express-validator');
+const User = require('../models/User');
 const router = Router();
 
-router.post('/register', async (req, res) => {
-  try {
-  } catch (e) {
-    res.status(500).json({ message: 'vse huovo' });
+router.post(
+  '/register',
+  [
+    check('email', 'huioviy email').isEmail(),
+    check('password', 'huioviy parole password').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(reg);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'huivie dannie',
+        });
+      }
+
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res.status(400).json({ message: 'uzhe takoi est' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: 'sozdan user' });
+    } catch (e) {
+      res.status(500).json({ message: 'vse huovo' });
+    }
   }
-});
+);
 
-router.post('/login', async (req, res) => {
-  try {
-  } catch (e) {}
-});
+router.post(
+  '/login',
+  [
+    check('email', 'zahuyar valid email').normalizeEmail().isEmail(),
+    check('password', 'pustoi password').exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(reg);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'huivie dannie pri logine',
+        });
+      }
 
-// router.post('/register', async (req, res) => {})
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'usera netu' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'huoviy parol poprobuy eshe' });
+      }
+      const token = jwt.sign(
+        {
+          userId: user.id,
+        },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      );
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: 'vse huovo' });
+    }
+  }
+);
 
 module.exports = router;
